116e7c575b19192e89079f4c0996d419b11ed0ab
githinjialbert <githinjialbert001@gmail.com>
2024-07-11 10:04:07 +0300

    update on the code

--- sessions.php
+++ sessions.php
@@ -8,7 +8,7 @@ session_set_cookie_params ([
     'lifetime' => 1800, 
     'domain' => 'localhost', 
     'path' => '/',
-    'secure' => 'true', 
+    'secure' => 'true',   
     'httponly' => 'true', 
 ]);
 
