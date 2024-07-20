732794d365521e2326bc220ae6e34498804e8c2d
githinjialbert <githinjialbert001@gmail.com>
2024-07-20 21:46:06 +0300

    trying to find the submission error

--- includes/blog_contr.inc.php
+++ includes/blog_contr.inc.php
@@ -33,9 +33,7 @@ class BlogControl {
                 // Call the method to handle comment submission
                 if ($this->master->takeUserInfo($username, $email, $comment_text)) {
                     echo "Thanks for your feedback. We'll reach out as soon as possible.";
-                } else {
-                    throw new Exception("There was an error in submitting your feedback.");
-                }
+                } 
             } catch (PDOException $e) {
                 throw new Exception("An error occurred. Your information could not be submitted successfully.");
             }
